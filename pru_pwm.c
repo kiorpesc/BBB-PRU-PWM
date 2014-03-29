9cbb262ee310a90fcb61f8fcc40d4a74ecb5921e
Charles Kiorpes <ckiorpes@gmail.com>
2014-03-29 20:02:12 +0000

    cleanup c code

--- pru_pwm.c
+++ pru_pwm.c
@@ -18,7 +18,7 @@
 
 // Driver header file
 #include "prussdrv.h"
-#include <pruss_intc_mapping.h>	 
+#include <pruss_intc_mapping.h>
 
 /******************************************************************************
 * Explicit External Declarations                                              *
@@ -38,7 +38,7 @@
 
 //memory addresses
 #define DDR_BASEADDR     0x80000000
-#define OFFSET_DDR	 0x00001000 
+#define OFFSET_DDR	 0x00001000
 #define OFFSET_SHAREDRAM 2048		//equivalent with 0x00002000
 
 #define PRUSS0_SHARED_DATARAM    4
@@ -84,15 +84,13 @@ void intHandler(int dummy){
 
 int main (void)
 {
-
-    
     unsigned int ret;
     tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-    
     printf("\nINFO: Starting single channel PWM on P9_27.\r\n");
+
     /* Initialize the PRU */
-    prussdrv_init ();		
-    
+    prussdrv_init ();
+
     /* Open PRU Interrupt */
     ret = prussdrv_open(PRU_EVTOUT_0);
     if (ret)
@@ -100,24 +98,24 @@ int main (void)
         printf("prussdrv_open open failed\n");
         return (ret);
     }
-    
+
     /* Get the interrupt initialized */
     prussdrv_pruintc_init(&pruss_intc_initdata);
 
     /* Initialize example */
     printf("\tINFO: Initializing PWM with default values.\r\n");
     //LOCAL_exampleInit(PRU_NUM);
-    
+
 
 //////////////////// LOAD VALUES INTO MEMORY /////////////////
-    void *DDR_regaddr1, *DDR_regaddr2, *DDR_regaddr3;	
+    void *DDR_regaddr1, *DDR_regaddr2, *DDR_regaddr3;
 
     /* open the device */
     mem_fd = open("/dev/mem", O_RDWR);
     if (mem_fd < 0) {
         printf("Failed to open /dev/mem (%s)\n", strerror(errno));
         return 1;
-    }	
+    }
 
     /* map the DDR memory */
     ddrMem = mmap(0, 0x0FFFFFFF, PROT_WRITE | PROT_READ, MAP_SHARED, mem_fd, DDR_BASEADDR);
@@ -136,25 +134,21 @@ int main (void)
     *(unsigned int*) DDR_regaddr2 = DUTY_CYCLES;
     *(unsigned int*) DDR_regaddr3 = STOP_FLAG;
 
-
-
-
-
     /* Execute example on PRU */
     printf("\tINFO: Starting PWM output on P9_27.\r\n");
-    prussdrv_exec_program (PRU_NUM, "./prucode.bin");
+    prussdrv_exec_program(PRU_NUM, "./prucode.bin"); // load pru firmware
 
     signal(SIGINT, intHandler);
 
-    // again, keep supposedly safe values for start (although this might arm some motors)
-    int last_input = 1060;
-    int input = 1060;
-    int converted = 212000;
+    // again, keep supposedly safe values for start (middle of range shouldn't arm anything)
+    int last_input = 1500;
+    int input = 1500;
+    int converted = 300000;
     while(input){
-        *(unsigned int*) DDR_regaddr2 = converted;
+        *(unsigned int*) DDR_regaddr2 = converted; // set register value in memory
         printf("Value entered: %i\n Value sent: %i\n", input, converted);
         last_input = input;
-        scanf(" %i", &input);
+        scanf(" %i", &input); // wait for new input
         if(input > 1010 && input < 1900){
             // sane value
             converted = input * 1000 / 5;  //input in microseconds converted to nanoseconds and then divided by 5 to get cycles
@@ -171,25 +165,11 @@ int main (void)
     printf("\tINFO: PRU completed transfer.\r\n");
     prussdrv_pru_clear_event (PRU0_ARM_INTERRUPT, PRU_EVTOUT_0);
 
-    /*
-    // Check if example passed
-    if ( LOCAL_examplePassed(PRU_NUM) )
-    {
-        printf("Example executed succesfully.\r\n");
-    }
-    else
-    {
-        printf("Example failed.\r\n");
-    }
-    */
-
     /* Disable PRU and close memory mapping*/
-    prussdrv_pru_disable(PRU_NUM); 
+    prussdrv_pru_disable(PRU_NUM);
     prussdrv_exit ();
     munmap(ddrMem, 0x0FFFFFFF);
     close(mem_fd);
 
     return(0);
 }
-
-
